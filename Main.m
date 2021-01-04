b5e42dcf62c9a189d9cb76d7faf6dc1059ce4163
lima1696 <55297721+lima1696@users.noreply.github.com>
2021-01-04 08:06:00 -0300

    Update Main.m

--- Main.m
+++ Main.m
@@ -13,7 +13,3 @@ nc = 1;
 options = [1e-2 500];
 
 [Jopt,Jfeas,Zopt,Zfeas] = GKN_Method(A,B,E,C,D,G,H,nc,options);
-
-%  [Transfer,Cost,Ac,Bc,Cc,Dc] = Full_Order_Output_Control(A,B,E,C,D,G,H);
-%  plot(length(Jopt),Cost,'ok','LineWidth',3);
-%  legend('GKN Method','Full Order Output Control');
